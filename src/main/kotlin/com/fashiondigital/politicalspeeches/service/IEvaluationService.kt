d6bdcf2efc3b883c287302263120c56984c1f9cd
burhan gunay <burhan.gunay@fairmoney.io>
2024-03-31 03:24:46 +0300

    v2 selected as a most recent and main branch, may additional unit tests added later

--- src/main/kotlin/com/fashiondigital/politicalspeeches/service/IEvaluationService.kt
+++ src/main/kotlin/com/fashiondigital/politicalspeeches/service/IEvaluationService.kt
@@ -4,5 +4,5 @@ import com.fashiondigital.politicalspeeches.model.EvaluationResult
 import com.fashiondigital.politicalspeeches.model.Speech
 
 interface IEvaluationService {
-    fun analyzeSpeeches(urls: List<Speech>): EvaluationResult
+    fun analyzeSpeeches(speeches: List<Speech>): EvaluationResult
 }
