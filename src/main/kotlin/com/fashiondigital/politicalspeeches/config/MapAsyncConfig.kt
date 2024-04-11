ec018b3f64e066e4649faf3b13ab7ba27e23992d
burhan gunay <burhan.gunay@fairmoney.io>
2024-04-11 12:06:48 +0300

    documentation added to mapAsync

--- src/main/kotlin/com/fashiondigital/politicalspeeches/config/MapAsyncConfig.kt
+++ src/main/kotlin/com/fashiondigital/politicalspeeches/config/MapAsyncConfig.kt
@@ -4,6 +4,15 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.withTimeout
 
+/**
+ * Maps the given [Iterable] of type [T] to a new [List] of type [R] using the provided [transformation] function.
+ * The mapping is performed asynchronously, allowing for concurrent execution of the transformation function.
+ * The operation will timeout after the specified [timeout] duration.
+ *
+ * @param transformation A suspend function that takes an element of type [T] and returns an element of type [R].
+ * @param timeout The maximum duration for the operation to complete before it times out.
+ * @return A new [List] of type [R] containing the results of the transformation function applied to each element of the input [Iterable].
+ */
 suspend fun <T, R> Iterable<T>.mapAsync(transformation: suspend (T) -> R, timeout: Long)
         : List<R> = withTimeout(timeout) {
     this@mapAsync
