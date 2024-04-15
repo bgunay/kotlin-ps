f108087c28375c7f088e8accd82f955874fb5002
burhan gunay <burhan.gunay@fairmoney.io>
2024-04-16 00:37:06 +0300

    added flux like usage of flow.

--- src/main/kotlin/com/fashiondigital/politicalspeeches/controller/EvaluationController.kt
+++ src/main/kotlin/com/fashiondigital/politicalspeeches/controller/EvaluationController.kt
@@ -12,9 +12,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -41,6 +45,8 @@ class EvaluationController(
      * will not be cancelled until all its child coroutines have completed.
      * The coroutineScope ensures that the entire process is completed
      * before the function returns, and it prevents the coroutine from being cancelled prematurely
+     *
+     * we can use coroutineScope in service layer also. But endpoint become sync
      */
     @Operation(
         summary = "Evaluates Politics in CSV file that you specify",
@@ -85,6 +91,32 @@ class EvaluationController(
         return flowOf(result)
     }
 
+    /*
+     * flux right usage
+     * map operator supports asynchronous operation (no need for flatMap)
+     * since it takes a suspending function parameter
+     * ref: https://spring.io/blog/2019/04/12/going-reactive-with-spring-coroutines-and-kotlin-flow
+     */
+    @GetMapping("flow", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+    suspend fun testFlow(): Flow<Int> {
+        val flow = flow {
+            emit(1)
+            delay(500)
+            emit(2)
+            delay(500)
+            emit(3)
+            delay(500)
+            emit(4)
+            delay(500)
+            emit(5)
+            delay(500)
+            emit(6)
+            delay(500)
+            emit(7)
+        }
+        return flow.map { it * 10 }
+    }
+
 
     /**
      * Evaluates Politics in CSV file that you specify. Finds most speeches, security and worldly politician.
@@ -97,10 +129,10 @@ class EvaluationController(
      */
     @GetMapping("evaluate3")
     suspend fun evaluate3(@RequestParam headers: Map<String, String>): ResponseEntity<EvaluationResult> {
-            val urlParams: Set<String> = ValidationUtil.extractAndValidateUrlsFromRequest(headers)
-            val csvData = csvHttpService.fetchCsvData(urlParams)
-            val speeches = csvParserService.parseCSV(csvData)
-            val result: EvaluationResult = evaluationService.analyzeSpeeches(speeches)
-            return ResponseEntity.ok(result)
-        }
+        val urlParams: Set<String> = ValidationUtil.extractAndValidateUrlsFromRequest(headers)
+        val csvData = csvHttpService.fetchCsvData(urlParams)
+        val speeches = csvParserService.parseCSV(csvData)
+        val result: EvaluationResult = evaluationService.analyzeSpeeches(speeches)
+        return ResponseEntity.ok(result)
+    }
 }
