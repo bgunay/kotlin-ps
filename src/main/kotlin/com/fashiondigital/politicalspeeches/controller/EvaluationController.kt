70c7b25d9549978e4968cff861649202ee20fbe9
burhan gunay <burhan.gunay@fairmoney.io>
2024-04-15 16:10:07 +0300

    added documentation

--- src/main/kotlin/com/fashiondigital/politicalspeeches/controller/EvaluationController.kt
+++ src/main/kotlin/com/fashiondigital/politicalspeeches/controller/EvaluationController.kt
@@ -20,7 +20,9 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-
+/**
+ * Evaluates Politics in CSV file that you specify. Finds most speeches, security and worldly politician.
+ */
 @RestController
 @Tag(name = "political-speech", description = "political-speech")
 class EvaluationController(
@@ -29,7 +31,17 @@ class EvaluationController(
     @Autowired val csvHttpService: ICsvHttpService,
 ) {
 
-
+    /**
+     * Evaluates Politics in CSV file that you specify. Finds most speeches, security and worldly politician.
+     *
+     * @param headers The headers containing the URLs of the CSV files to be evaluated.
+     * @return The evaluation result of the specified CSV files.
+     *
+     * coroutineScope ensures that the coroutine it contains
+     * will not be cancelled until all its child coroutines have completed.
+     * The coroutineScope ensures that the entire process is completed
+     * before the function returns, and it prevents the coroutine from being cancelled prematurely
+     */
     @Operation(
         summary = "Evaluates Politics in CSV file that you specify",
         description = "Finds most speeches, security and worldly politician."
@@ -54,7 +66,14 @@ class EvaluationController(
         result
     }
 
-    //Flow returns List
+    /**
+     * Evaluates Politics in CSV file that you specify. Finds most speeches, security and worldly politician.
+     *
+     * @param headers The headers containing the URLs of the CSV files to be evaluated.
+     * @return A Flow of the evaluation result of the specified CSV files.
+     *  Flow returns List, its flux equivalent in coroutines world.
+     *  fun handler(): Flux<T> becomes fun handler(): Flow<T>
+     */
     @GetMapping("evaluate2")
     suspend fun evaluate2(@RequestParam headers: Map<String, String>): Flow<EvaluationResult>? {
         val urlParams: Set<String> = ValidationUtil.extractAndValidateUrlsFromRequest(headers)
@@ -65,6 +84,15 @@ class EvaluationController(
     }
 
 
+    /**
+     * Evaluates Politics in CSV file that you specify. Finds most speeches, security and worldly politician.
+     *
+     * @param headers The headers containing the URLs of the CSV files to be evaluated.
+     * @return The evaluation result of the specified CSV files wrapped in a ResponseEntity.
+     *
+     * fun handler(): Mono<Void> becomes suspend fun handler()
+     *
+     */
     @GetMapping("evaluate3")
     suspend fun evaluate3(@RequestParam headers: Map<String, String>): ResponseEntity<EvaluationResult> {
             val urlParams: Set<String> = ValidationUtil.extractAndValidateUrlsFromRequest(headers)
